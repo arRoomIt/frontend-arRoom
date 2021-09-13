93f246be6a4d47a2c6a05abf26378d9883a5f474
Mohibul <tuhin28012@gmail.com>
2021-09-13 21:27:12 +0200

    FIX: added chakraprovider in index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
+import { ChakraProvider } from "@chakra-ui/react";
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ChakraProvider>
     <App />
-  </React.StrictMode>,
+  </ChakraProvider>,
   document.getElementById('root')
 );
 
