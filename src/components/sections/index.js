e48419f191e0958d538127fa75192f7a2a16c0df
Javier9614 <javierarandalopez96@gmail.com>
2021-09-16 11:36:02 +0200

    FEAT: create component footer and added in ListLayout,HomeLayout and index of section

--- src/components/sections/index.js
+++ src/components/sections/index.js
@@ -3,11 +3,15 @@ import Hero from './Hero';
 import Host from './Host';
 import Slider from './Slider';
 import Card from './Card';
+import Paginacion from './Paginacion';
+import Footer from './Footer';
 
 export {
     Header,
     Hero,
     Host,
     Slider,
-    Card
+    Card,
+    Paginacion,
+    Footer
 }
