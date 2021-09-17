261509765d5101a0b183a2da1e719a0626c69d06
Juan José Trigo Hormigos <trigo.hormigos@bootcamp-upgrade.com>
2021-09-17 19:12:16 +0200

    FEAT: add function to find worspace for id

--- src/api/WorkspaceApi.js
+++ src/api/WorkspaceApi.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-const BASE_URL = "http://localhost:5000/workspace";
+const BASE_URL = "http://localhost:3000/workspace";
 const postUrl = `${BASE_URL}/create`;
 const editUrl = `${BASE_URL}/edit`;
 const idUrl = `${BASE_URL}`;
@@ -17,4 +17,22 @@ export const getListWorkspace = async () => {
   }
 
   return data;
+};
+
+export const getWorkspaceById = async (id) => {
+  
+  const res = await fetch(`${BASE_URL}/${id}`,{
+    method: 'GET',
+  });
+
+  const data = await res.json();
+
+  console.log('getWorkspaceById',data);
+
+  if (!res.ok) {
+    throw new Error(res.message);
+  }
+
+  return data;
+  
 };
