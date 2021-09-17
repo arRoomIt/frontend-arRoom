8c57f4dca75c30dde9e80640972eec7b55052050
Mohibul <tuhin28012@gmail.com>
2021-09-17 17:48:49 +0200

    STYLE: register form

--- src/components/sections/Register/RgisterUser.jsx
+++ src/components/sections/Register/RgisterUser.jsx
@@ -2,81 +2,103 @@ import { Button, chakra, FormControl, FormLabel, Input, Stack } from '@chakra-ui
 import { PasswordField } from './PasswordField'
 
 import React , { useState,useContext } from 'react'
-import { useHistory } from 'react-router-dom';
 
-import { loginApi } from '../../../api/auth.api';
-import { UserContext } from '../../../auth/UserContext';
+import { useFormik } from "formik";
 
-const INITIAL_STATE = {
-  name:'',
-  email: '',
-  password: '',
-}
+import './RgisterUser.scss';
 
 function RgisterUser(props) {
-  const history = useHistory();
-  const [setUserContext] = useContext(UserContext);
-  const [state, setState] = useState(INITIAL_STATE);
 
+  const validate = (values) => {
+    const errors = {};
 
-  const inputChange = (event) => {
-    const { name, value } = event.target;
-    setState({ ...state, [name]: value });
+    if (!values.email) {
+      errors.email = "Required";
+    } else {
+      console.error("Hola???")
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      const test = re.test(String(values.email).toLowerCase());
+      if(!test){
+        errors.email = "It's not a valid email";
+      }
+    }
+
+    if (!values.password) {
+      errors.password = "Required";
+    } else if (values.password.length < 8) {
+      errors.password = "Must be 8 characters or more";
+    } else if (values.password === "12345678") {
+      errors.password = "Must not be 12345678 !!!";
+    }
+
+    if (!values.repassword) {
+      errors.repassword = "Required";
+    } else if (values.repassword !== values.password) {
+      errors.repassword = "Second password doesn't match";
+    }
+
+    return errors;
   };
 
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      repassword: "",
+      isHost: false,
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
     return (
-      <chakra.form
-      onSubmit={async (e) => {
-        console.log(state)
-        e.preventDefault();
+      <div>
+      <form  onSubmit={formik.handleSubmit}>
+        <FormLabel>Email</FormLabel>
+        <Input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div className="error">{formik.errors.email}</div>
+        ) : null}
+        <FormLabel htmlFor="password">Password</FormLabel>
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.password}
+        />
+        {formik.touched.password && formik.errors.password ? (
+          <div className="error">{formik.errors.password}</div>
+        ) : null}
 
-        try {
+        <FormLabel htmlFor="repassword">Repeat Password</FormLabel>
+        <Input
+          id="repassword"
+          name="repassword"
+          type="password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.repassword}
+        />
+        {formik.touched.repassword && formik.errors.repassword ? (
+          <div className="error">{formik.errors.repassword}</div>
+        ) : null}
 
-          const form = {
-            email: e.target.email.value,
-            password: e.target.password.value,
-          }
-
-          // setUserContext(user);
-          // history.push('/');
-
-
-        } catch (error) {
-          console.log(error);
-        }
-
-
-      }}
-      {...props}
-    >
-      <Stack spacing="6">
-        <FormControl id="name">
-          <FormLabel>Name</FormLabel>
-          <Input
-            name="name"
-            type="name"
-            value={state.name}
-            autoComplete="name"
-            onChange={inputChange}
-            required />
-        </FormControl>
-
-        <FormControl id="email">
-          <FormLabel>Email address</FormLabel>
-          <Input
-            name="email"
-            type="email"
-            value={state.email}
-            autoComplete="email"
-            onChange={inputChange}
-            required />
-        </FormControl>
-        <PasswordField />
-        <Button type="submit" colorScheme="blue" size="lg" fontSize="md">
-          Sign In
-        </Button>
-      </Stack>
-    </chakra.form>
+          <Button type="submit" colorScheme="blue" size="lg" fontSize="md">
+            Sign Up
+          </Button>
+      </form >
+    </div>
     )
 }
 
