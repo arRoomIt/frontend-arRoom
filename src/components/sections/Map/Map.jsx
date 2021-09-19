6c20577e5be9481c6490817cee2c21f25e266f9f
Mohibul <tuhin28012@gmail.com>
2021-09-19 18:44:29 +0200

    FEAT: workspace marker in leaflet is working

--- src/components/sections/Map/Map.jsx
+++ src/components/sections/Map/Map.jsx
@@ -1,37 +1,27 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup, MapConsumer } from 'react-leaflet';
 import './Map.css';
-import L from 'leaflet';
+
+import { Box } from "@chakra-ui/react"
 
 
-function Map() {
+function Map(props) {
 
-                // const myIcon = L.icon({
-                //     iconUrl: require('../../../assets/images/icon.svg'),
-                //     iconSize: [64,64],
-                //     iconAnchor: [32, 64],
-                //     popupAnchor: null,
-                //     shadowUrl: null,
-                //     shadowSize: null,
-                //     shadowAnchor: null
-                // });
+
     return (
-        <div className="map-container">
-            {/* <MapContainer center={{lat: '40.2839', lng: '-3.80033'}} zoom={13} scrollWheelZoom={true}> */}
-            <MapContainer center={{lat: '40.7903', lng: '-1.3541'}} zoom={13} scrollWheelZoom={true}>
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker  position={{lat: '40.2839', lng: '-3.80033'}}>
-            <Marker  position={{lat: '40.7903', lng: '-1.3541'}}>
-            
-            
-            </Marker>
-            </Marker>
-            </MapContainer>
-        </div>
+        <Box mt="6" mb="10">
+            <div className="map-container">
+
+                <MapContainer center={{ lat: `${props.lat}`, lng: `${props.lng}` }} zoom={13} scrollWheelZoom={true}>
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={{ lat: `${props.lat}`, lng: `${props.lng}` }}></Marker>
+                </MapContainer>
+            </div>
+        </Box>
     )
 }
 
-export default Map ;
+export default Map;
--- src/pages/DetailWorkspace/DetailWorkspace.jsx
+++ src/pages/DetailWorkspace/DetailWorkspace.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { Map, CalendarDetail,Reserve } from '../../components/sections';
-import { getWorkspaceById } from  '../../api/WorkspaceApi'
+import { Map, Reserve,Footer } from '../../components/sections';
+import { getWorkspaceById } from '../../api/WorkspaceApi'
 import { useLocation } from 'react-router-dom';
-import { Image, Box, Grid} from "@chakra-ui/react";
+import { Image, Box, Grid } from "@chakra-ui/react";
 import './DetailWorkspace.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUserFriends, faBookOpen, faDoorOpen, faFrown, faSmile, faMoneyBillWave, faTimes, faCheck } from '@fortawesome/free-solid-svg-icons';
@@ -14,88 +14,78 @@ function DetailWorkspace() {
     const location = useLocation();
     const divitions = location.pathname.split("/", 3);
     const workSpaceId = divitions[2];
-    
-    const[detail, setDetail] = useState({});
+
+    const [detail, setDetail] = useState({});
     const [change, setChange] = useState(false);
-    
 
-    
+
+
     useEffect(() => {
-    if(workSpaceId !== undefined){
-    getWorkspaceById(workSpaceId).then(result => {
-        setDetail(result);
-        setChange(false); 
-    })
-}
-}, [change, workSpaceId]);
+        if (workSpaceId !== undefined) {
+            getWorkspaceById(workSpaceId).then(result => {
+                setDetail(result);
+                setChange(false);
+            })
+        }
+    }, [change, workSpaceId]);
 
-    
-    
+
+
     return (
         <div>
-        <div className="boxImage">
-        <Box maxW="lg" borderWidth="1px" borderRadius="lg" overflow="hidden">
-            <Image src={detail.images} />
-        </Box>
-        </div>
-        <Grid
-            container
-            direction="col"
-            justifyContent="center"
-            alignItems="center">
-    <div className="boxText">
-        
-    <div class="row">
-        <div class="col">
-
-        
-            <p className="bone"><FontAwesomeIcon icon={faHome}/>&nbsp;Workspace</p>
-            <p className="btitle">{detail.title}</p>
-            <p className="btop"><FontAwesomeIcon icon={faUserFriends}/>&nbsp;Occupancy</p>
-            <p>{detail.totalOccupancy}</p>
-            <p className="btop"><FontAwesomeIcon icon={faDoorOpen}/>&nbsp;Workspace Type</p>
-            <p>{detail.roomType}</p>
-            <p className="btop"><FontAwesomeIcon icon={faMoneyBillWave}/>&nbsp;Price</p>
-            <p>{detail.price}€</p>
-            <p className="btop">Heating:&nbsp; 
-                {detail.hasAirHeating === true ? <FontAwesomeIcon className="green" icon={faCheck}/> : <FontAwesomeIcon className="red" icon={faTimes}/>}
-            </p>
-            <p className="btop">Air Conditioning:&nbsp; 
-                {detail.hasAirCon === true ? <FontAwesomeIcon className="green" icon={faCheck}/> : <FontAwesomeIcon className="red" icon={faTimes}/>}
-            </p>
-            <p className="btop">Internet:&nbsp; 
-                {detail.hasInternet === true ? <FontAwesomeIcon  className="green" icon={faCheck}/> : <FontAwesomeIcon className="red" icon={faTimes}/>}
-            </p>
-            <p className="bsummary"><FontAwesomeIcon icon={faBookOpen}/>&nbsp;Description</p>
-            <p>{detail.summary}</p>
-        
-        </div>
-        <div class="col">
-                {/* <h1>Botón?????</h1> */}
-                <Reserve detail={detail}/>
+            <div className="boxImage">
+                <Box maxW="lg" borderWidth="1px" borderRadius="lg" overflow="hidden">
+                    <Image src={detail.images} />
+                </Box>
             </div>
+            <Grid
+                container
+                direction="col"
+                justifyContent="center"
+                alignItems="center">
+                <div className="boxText">
+
+                    <div class="row">
+                        <div class="col">
+
+
+                            <p className="bone"><FontAwesomeIcon icon={faHome} />&nbsp;Workspace</p>
+                            <p className="btitle">{detail.title}</p>
+                            <p className="btop"><FontAwesomeIcon icon={faUserFriends} />&nbsp;Occupancy</p>
+                            <p>{detail.totalOccupancy}</p>
+                            <p className="btop"><FontAwesomeIcon icon={faDoorOpen} />&nbsp;Workspace Type</p>
+                            <p>{detail.roomType}</p>
+                            <p className="btop"><FontAwesomeIcon icon={faMoneyBillWave} />&nbsp;Price</p>
+                            <p>{detail.price}€</p>
+                            <p className="btop">Heating:&nbsp;
+                                {detail.hasAirHeating === true ? <FontAwesomeIcon className="green" icon={faCheck} /> : <FontAwesomeIcon className="red" icon={faTimes} />}
+                            </p>
+                            <p className="btop">Air Conditioning:&nbsp;
+                                {detail.hasAirCon === true ? <FontAwesomeIcon className="green" icon={faCheck} /> : <FontAwesomeIcon className="red" icon={faTimes} />}
+                            </p>
+                            <p className="btop">Internet:&nbsp;
+                                {detail.hasInternet === true ? <FontAwesomeIcon className="green" icon={faCheck} /> : <FontAwesomeIcon className="red" icon={faTimes} />}
+                            </p>
+                            <p className="bsummary"><FontAwesomeIcon icon={faBookOpen} />&nbsp;Description</p>
+                            <p>{detail.summary}</p>
+
+                        </div>
+                        <div class="col">
+                            <Reserve detail={detail} />
+                        </div>
+                    </div>
+
+                </div>
+            </Grid>
+            {detail.latitude !==undefined &&
+                <Map lat={detail.latitude} lng={detail.longitude} />
+            }
+            <Footer/>
         </div>
-        
-    </div>
-          </Grid>
-            <Map />
-            {/* <CalendarDetail/> */}
-    </div>
-    
+
     )
 
-    
+
 }
 
 export default DetailWorkspace
-
-
-
-{/* <div class="row">
-  <div class="col">
-    Ancho del 50%
-  </div>
-  <div class="col">
-    Ancho del 50%
-  </div>
-</div> */}
