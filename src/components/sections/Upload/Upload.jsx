b43520e31270f0f12550feb3f03463b9c05c84a1
Juan José Trigo Hormigos <trigo.hormigos@bootcamp-upgrade.com>
2021-09-20 03:04:33 +0200

    FEAT: added message in upload image profile

--- src/components/sections/Upload/Upload.jsx
+++ src/components/sections/Upload/Upload.jsx
@@ -1,9 +1,9 @@
-import React,{useState,useContext} from "react";
+import React,{ useState, useContext, useEffect } from "react";
 
 import {uploadImage as uploadProfileImage} from '../../../api/user.api'
 import { UserContext } from '../../../auth/UserContext';
 import './Upload.css';
-import { Flex, Center, Text } from "@chakra-ui/react";
+import { Flex, Center, Text, useToast } from "@chakra-ui/react";
 
 function Upload() {
 
@@ -12,6 +12,19 @@ function Upload() {
     const [fileInputState, setFileInputState] = useState("");
 
     const [previewSource,setPreviewSource] = useState();
+
+    const toast = useToast()
+    const [incorrectToast, setIncorrectToast] = useState(false);
+    const [sucess, setSucess] = useState(false);
+
+
+    useEffect(() => {
+        setIncorrectToast(false);
+    }, [incorrectToast]);
+
+    useEffect(() => {
+        setSucess(false)
+    }, [sucess]);
 
     const handleFileInput = (e) => {
         const file = e.target.files[0];
@@ -42,21 +55,32 @@ function Upload() {
                 userId: userContext._id,
                 image: base64EncodedImage
             }
+            setSucess(true);
             console.log("upload--->",userContext);
             uploadProfileImage(form)
-                .then((result)=>{
-                    setUserContext(result)
-                })
-                .catch((error) => {console.error(error)})
+            .then((result)=>{
+                setUserContext(result)
+            })
+            .catch((error) => {console.error(error)})
         } catch (error) {
             console.error(error);
         }
     }
   
     return (
+        <div>
+            {sucess &&
+                toast({
+                    position: "top",
+                    title: "You are very handsome! 😍😍",
+                    description: 'Your profile image has been uploaded successfully!',
+                    status: "success",
+                    duration: 6000,
+                    isClosable: true,
+                })
+            }
       <Flex >
-        
-      <Center w="70%" ml="12" borderRadius="15px">
+        <Center w="70%" ml="12" borderRadius="15px">
         <Text><div className="FileUpload">
             <form onSubmit={handleSubmitFile}>
                 <input className="binput" type="file" name="image" 
@@ -65,12 +89,13 @@ function Upload() {
             {previewSource && 
                 <img className="bimage" src={previewSource} alt="" style={{width:"250px", height:"250px"}}/>
             }
-                <button  className="binput__sub" type="submit">Submit</button>
-            </form> 
+                <button className="binput__sub" type="submit">Submit</button>
+            </form>                                                          
         </div>
         </Text>
     </Center>
       </Flex>
+      </div>
     );
   };
 
